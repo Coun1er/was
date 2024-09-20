8704772cf8defd43e5eb19deef6f8d8a28e60e1c
coun1er <coun1er@users.noreply.github.com>
2024-09-20 11:15:20 +0300

    fix: upd

--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ from aiogram.types import BufferedInputFile
 
 
 def generate_account_list_file(order_id: str, custom_messages: str, goods: list):
+    mkd_text = ""
     goods_text_preview = "\n\nprivate_seed:email_login:email_pass\n\n"
     goods_text = "\n".join(
         [f"{g['seed']}:{g['email_login']}:{g['email_pass']}" for g in goods]
