5b304b585e1dd805bfd0374da937d38e62514e98
coun1er <coun1er@users.noreply.github.com>
2024-09-20 11:21:06 +0300

    fix: добавил очистку неиспользованных докер контенеров в workflows гита и чуток поправил формирование файла со списком аккаунтов

--- helper.py
+++ helper.py
@@ -2,15 +2,14 @@ from aiogram.types import BufferedInputFile
 
 
 def generate_account_list_file(order_id: str, custom_messages: str, goods: list):
-    mkd_text = ""
-    goods_text_preview = "\n\nprivate_seed:email_login:email_pass\n\n"
+    goods_text_preview = "- private_seed:email_login:email_pass\n"
     goods_text = "\n".join(
         [f"{g['seed']}:{g['email_login']}:{g['email_pass']}" for g in goods]
     )
 
     description_message = f"Заказ: {str(order_id)}\nФормат выдачи: private_seed:email_login:email_pass и просто private_seed\n"
 
-    goods_text_only_private_seed_preview = "\n\nprivate_seed\n\n"
+    goods_text_only_private_seed_preview = "\n\n- private_seed\n"
     goods_text_only_private_seed = "\n".join([f"{g['seed']}" for g in goods])
 
     full_text = (
